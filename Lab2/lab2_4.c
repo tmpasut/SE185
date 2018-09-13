556f2f1646c508cf3029d0add22ca7599ddd04d7
Pasut <tmpasut@iastate.edu>
2018-09-13 18:14:43 -0500

    Added the existing files

---
+++ Lab2/lab2_4.c
@@ -0,0 +1,18 @@
+// Lab 2
+// SE 185
+// Name: Theresa Pasut
+// Section: A
+// Date: 9/7/2018
+
+	printf("a. int = 6427 + 1725 = ")
+	printf(b. int = (6427 * 3925) - 95 = ")
+	printf(c. double = 79 + 12 / 5 = ")
+	printf(d. double = 3640.0 / 107.9 = ")
+	printf(e. int = (22 / 3) * 3 = ")
+	printf(f. int = 22 / (3 * 3) = ")
+	printf(g. double = 22 / (3 * 3) = ")
+	printf(h. double = 22 / 3 * 3 = ")
+	printf(i. double = (22.0 / 3) * 3.0 = ")
+	printf(j. int = 22.0 / 3.0 * 3.0 = ")
+	printf(k. double = 22.0 / 3.0 * 3.0 = ")
+	
